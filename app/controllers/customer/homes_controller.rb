fa0121b1cfc57222dd8c7483d645b8d938291771
S-kuwano <tukushi14@gmail.com>
2021-07-17 14:27:25 +0000

    [Add]top-page

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,7 +1,9 @@
 class  Customer::HomesController < ApplicationController
   def top
+    @products = Product.page(params[:page]).per(4).reverse_order
+    @genres = Genre.all
   end
-  
+
   def about
   end
 end
