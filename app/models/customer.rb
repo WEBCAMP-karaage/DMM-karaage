8510888dc53117c431abbcc6a734df5790504209
S-kuwano <tukushi14@gmail.com>
2021-07-18 11:10:18 +0000

    [Add]shipping_address

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :shipping_address, dependent: :destroy
 
   def active_for_authentication?
     super && (self.withdrawal_flag == false)
   end
+  
 end
