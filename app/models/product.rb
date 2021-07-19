9dc614526c0ed5fe699097ffe12c9830a81688ff
EC2 Default User <ec2-user@ip-172-31-7-203.ec2.internal>
2021-07-19 06:53:57 +0000

    cart-products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
     attachment :image, destroy: false
     has_many :cart_products, dependent: :destroy
+    #税込み
     def  add_tax_price
       (self.non_taxed_price * 1.10).round
     end
+    
 end
