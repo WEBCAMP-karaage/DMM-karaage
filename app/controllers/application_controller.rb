c44cc5edeb8b7c149de82f1356fc2ac2962c5daa
S-kuwano <tukushi14@gmail.com>
2021-07-25 02:45:02 +0000

    [Update]header-search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,9 +27,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_out_path_for(resource_or_scope)
-    return new_admin_session_path if resource_or_scope == :admin
-    root_path
-  end
-
 end
